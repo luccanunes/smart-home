0c92db75f6fa8e141603187c8a5ef74e8ad07cb3
Lucca Nunes <lucca.nunes@ifood.com.br>
2026-06-01 14:55:54 -0300

    Initialize API with patch state endpoint

--- api/src/server.py
+++ api/src/server.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from classes import Lamp, LampState
+from utils import read_config
 
 app = FastAPI(title="Smart Home API")
 
+lamp_dict = {lamp_data["device_id"]: Lamp(lamp_data) for lamp_data in read_config()}
+
 @app.patch("/lamps/{lamp_id}/state")
 def update_lamp_status(lamp_id: str, state: LampState):
-    return {"status": "Servidor online", "sistema": "Tuya Local"}
+    if lamp_id not in lamp_dict:
+        raise HTTPException(status_code=404, detail="Lamp not found. Make sure device id is valid.")
+    lamp_dict[lamp_id].state = state
+    return {
+        "message": "Update successful",
+        "new_state": lamp_dict[lamp_id].state
+    }
