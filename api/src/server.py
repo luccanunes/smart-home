8328cf47f4412bb30d1a0c4708b2ecf05eaa9696
Lucca Nunes <lucca.nunes@ifood.com.br>
2026-06-10 13:17:59 -0300

    Write startup code correctly

--- api/src/server.py
+++ api/src/server.py
@@ -1,17 +1,30 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
 from api.src.classes import Lamp, LampState
 from api.src.utils import read_config
 
-app = FastAPI(title="Smart Home API")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("INICIALIZANDO APARELHOS (Garantido apenas 1 vez)")
+    
+    app.state.lamps = {lamp_data["device_id"]: Lamp(lamp_data) for lamp_data in read_config()}
+    
+    yield
+    
+    print("Shutting down server")
 
-lamp_dict = {lamp_data["device_id"]: Lamp(lamp_data) for lamp_data in read_config()}
+
+app = FastAPI(title="Smart Home API", lifespan=lifespan)
+
 
 @app.patch("/lamps/{lamp_id}/state")
 def update_lamp_status(lamp_id: str, state: LampState):
+    lamp_dict = app.state.lamps
     if lamp_id not in lamp_dict:
         raise HTTPException(status_code=404, detail="Lamp not found. Make sure device id is valid.")
     lamp_dict[lamp_id].state = state
     return {
         "message": "Update successful",
+        "online": lamp_dict[lamp_id].online,
         "new_state": lamp_dict[lamp_id].state
     }
