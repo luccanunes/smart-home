5a00cb80f92f0a5188a71f22238b88aef620dbc3
Lucca Nunes <lucca.nunes@ifood.com.br>
2026-06-10 22:22:25 -0300

    Use new error handling

--- api/src/server.py
+++ api/src/server.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
-from api.src.classes import Lamp, LampState
+from api.src.classes import Lamp, LampState, DeviceOfflineError
 from api.src.utils import read_config
 
 @asynccontextmanager
@@ -20,9 +20,15 @@ app = FastAPI(title="Smart Home API", lifespan=lifespan)
 @app.patch("/lamps/{lamp_id}/state")
 def update_lamp_status(lamp_id: str, state: LampState):
     lamp_dict = app.state.lamps
+
     if lamp_id not in lamp_dict:
-        raise HTTPException(status_code=404, detail="Lamp not found. Make sure device id is valid.")
-    lamp_dict[lamp_id].state = state
+        raise HTTPException(status_code=404, detail="Device not found. Make sure device id is valid.")
+    
+    try:
+        lamp_dict[lamp_id].state = state
+    except DeviceOfflineError as e:
+        raise HTTPException(status_code=503, detail=f"Failed to update device state. It is likely offline: {e}",)
+
     return {
         "message": "Update successful",
         "device_id": lamp_dict[lamp_id].device_id,
